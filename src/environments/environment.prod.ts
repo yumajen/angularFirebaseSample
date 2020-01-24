48b3c0a531a08ae69a6cc684eef335b528cf7d6e
yumajen <okabe.yuma@jenesty.co.jp>
2020-01-24 09:22:19 +0900

    Firebaseへのデプロイ準備

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCZjOwReN4Yu6AHcxP8VxXW8fQFvQupQ6E",
+    authDomain: "my-angular-project-2.firebaseapp.com",
+    databaseURL: "https://my-angular-project-2.firebaseio.com",
+    projectId: "my-angular-project-2",
+    storageBucket: "my-angular-project-2.appspot.com",
+    messagingSenderId: "747818716893",
+    appId: "1:747818716893:web:096163cf2e0b11aa6b1534",
+    measurementId: "G-8J4KSX88BD"
+  },
 };
